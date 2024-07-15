4c320bb38b33b153a4b9941a664b35bca253a804
Vasily Shuklin <53554193+MyAwesomeGit@users.noreply.github.com>
2024-07-15 19:48:26 +0500

    Delete MarketMomentum/Model/ struct Stock- Codable, Identifiable {     let id- String     let symbol- String     let description- String     let price- Double } .swift
    Remove deprecated file

--- MarketMomentum/Model/ struct Stock- Codable, Identifiable {     let id- String     let symbol- String     let description- String     let price- Double } .swift
+++
@@ -1,8 +0,0 @@
-//
-//   struct Stock- Codable, Identifiable {     let id- String     let symbol- String     let description- String     let price- Double } .swift
-//  MarketMomentum
-//
-//  Created by Vasily Shuklin on 13.07.2024.
-//
-
-import Foundation
